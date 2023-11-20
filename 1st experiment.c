c208d421c86606336adc4e1c5e3f9c505175f56c
Solonani <119772084+Solonani@users.noreply.github.com>
2023-11-20 11:16:56 +0530

    Create 1st experiment.c

---
+++ 1st experiment.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	printf("Name:m.thirumala manohar\nReg.no: 192211851");
+	printf("\nEnter last number of series:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		printf("%d\n",i);
+	}
+	return 0;
+}
